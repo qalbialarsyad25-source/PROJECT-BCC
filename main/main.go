4611660ad05a88ff4e157b209a4de068dd155747
Muhammad Qalbi Al Arsyad <qalbialarsyad25@gmail.com>
2026-04-02 14:05:13 +0700

    fix: cors

--- main/main.go
+++ main/main.go
@@ -3,25 +3,10 @@ package main
 import (
 	"bcc-geazy/config"
 	"bcc-geazy/internal/app"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-	"github.com/rs/cors"
 )
 
 func main() {
 	config.NewConfig()
 	app.Run()
-
-	r := gin.Default()
-	r.Use(cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000", "https://geazy.vercel.app/"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Content-Type", "Authorization"},
-		AllowCredentials: true,
-	}))
-
-	handler := c.Handler(mux)
-	http.ListenAndServe(":8080", handler)
 
 }
