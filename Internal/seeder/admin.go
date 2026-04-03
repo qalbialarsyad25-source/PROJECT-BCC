d77a30a12c8b0e818f0a21a0c62ece9cb6163baa
Muhammad Qalbi Al Arsyad <qalbialarsyad25@gmail.com>
2026-04-03 18:56:42 +0700

    fix: akun admin

--- Internal/seeder/admin.go
+++ Internal/seeder/admin.go
@@ -4,22 +4,20 @@ import (
 	"bcc-geazy/internal/entity"
 	"bcc-geazy/internal/repository"
 	"context"
-	"os"
 
 	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func SeedAdmin(repo repository.IUserRepository) {
-	adminEmail := os.Getenv("EMAIL_ADMIN")
-	adminSandi := os.Getenv("SANDI_ADMIN")
+	adminEmail := "admin28@gmail.com"
 
 	existing, _ := repo.GetUserByEmail(context.Background(), adminEmail)
 	if existing != nil {
 		return
 	}
 
-	hash, _ := bcrypt.GenerateFromPassword([]byte(adminSandi), bcrypt.DefaultCost)
+	hash, _ := bcrypt.GenerateFromPassword([]byte("admin282828"), bcrypt.DefaultCost)
 
 	admin := entity.User{
 		Id:       uuid.New(),
